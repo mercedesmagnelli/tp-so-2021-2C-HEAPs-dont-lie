09d35044d2f3eeee1609b921035291b5e8264f6c
Eruizmagliano <eruizmagliano@gmail.com>
2021-11-11 11:31:13 -0300

    mas pruebas

--- matelib/src/main.c
+++ matelib/src/main.c
@@ -27,12 +27,36 @@ int main(int argc, char** argv) {
 
 		mate_init(lib_ref, string_from_format("Proceso %d", n));
 
-		if (rand() % 10 > 5) {
+		if (rand()%10>5) {
+
 			mate_sem_wait(lib_ref, "SEM_BBB");
 		}
+
 		mate_sem_wait(lib_ref, "SEM_CCC");
 
 		mate_sem_wait(lib_ref, "SEM_AAA");
+
+		loggear_trace("[PID: %zu] - En 10 segundos, hace SEM_POST", ((t_instance_metadata *) lib_ref->group_info)->pid);
+
+		sleep(10);
+
+		mate_sem_post(lib_ref, "SEM_AAA");
+	}
+
+	void multi_hilo2(int * n) {
+		mate_instance * lib_ref = malloc(sizeof(mate_instance));
+
+		mate_init(lib_ref, string_from_format("Proceso %d", n));
+
+		mate_sem_wait(lib_ref, "SEM_AAA");
+
+		mate_sem_wait(lib_ref, "SEM_CCC");
+
+		if (rand()%10>5) {
+
+			mate_sem_wait(lib_ref, "SEM_BBB");
+		}
+
 
 		loggear_trace("[PID: %zu] - En 10 segundos, hace SEM_POST", ((t_instance_metadata *) lib_ref->group_info)->pid);
 
@@ -48,36 +72,34 @@ int main(int argc, char** argv) {
 	mate_sem_init(referencia, "SEM_BBB", 10);
 	mate_sem_init(referencia, "SEM_CCC", 10);
 
-	for (int i = 1; i < 5; ++i) {
-		pthread_t thread;
+	pthread_t thread_uno;
+	pthread_t thread_dos;
+	pthread_t thread_tres;
+	pthread_t thread_cuatro;
 
-		pthread_create(&thread, NULL, (void *) multi_hilo1, &i);
-	}
+	int uno = 1;
+	int dos = 2;
+	int tres = 3;
+	int cuatro = 4;
+
+	pthread_create(&thread_uno, NULL, (void *) multi_hilo1, &uno);
+	pthread_create(&thread_dos, NULL, (void *) multi_hilo1, &dos);
+	pthread_create(&thread_tres, NULL, (void *) multi_hilo2, &tres);
+	pthread_create(&thread_cuatro, NULL, (void *) multi_hilo2, &cuatro);
 
 
-	loggear_debug("En 30 segundos, Se destruye el semaforo");
-	sleep(30);
+	loggear_debug("Esperamos a que finalicen todos");
+
+	pthread_join(thread_uno, NULL);
+	pthread_join(thread_dos, NULL);
+	pthread_join(thread_tres, NULL);
+	pthread_join(thread_cuatro, NULL);
+
 	mate_sem_destroy(referencia, "SEM_AAA");
 	mate_sem_destroy(referencia, "SEM_BBB");
 	mate_sem_destroy(referencia, "SEM_CCC");
 
-
-	sleep(5);
-	mate_sem_init(lib_ref, "sem uno", 1);
-	sleep(5);
-	mate_sem_wait(lib_ref, "sem uno");
-	sleep(5);
-	mate_sem_post(lib_ref, "sem uno");
-	sleep(5);
-	mate_call_io(lib_ref, "hierbitas", "puto");
-	sleep(5);
-	mate_sem_destroy(lib_ref, "sem uno");
-	sleep(5);
-	mate_close(lib_ref);
-
-
-	sleep(100);
-
+	mate_close(referencia);
 
 /*
 	t_instance_metadata* metadata = malloc(sizeof(t_instance_metadata));
