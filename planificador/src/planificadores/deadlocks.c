c33148e6c4d88be9d7e8c5f569d45e1e85d903e3
Alexander Armua <alexanderarmua1@gmail.com>
2021-11-13 16:39:01 -0300

    Algoritmo DFS

--- planificador/src/planificadores/deadlocks.c
+++ planificador/src/planificadores/deadlocks.c
@@ -11,7 +11,7 @@ t_dictionary * dict_visitados;
 void iniciar_evaluacion_deadlock() {
 	colas_bloquear_listas_bloqueados();
 
-	t_list * list_bloqueados = colas_obtener_listas_bloqueados(SEMAFORO);
+	list_bloqueados = colas_obtener_listas_bloqueados(SEMAFORO);
 
 	size_bloqueados = list_size(list_bloqueados);
 
@@ -44,32 +44,32 @@ bool esta_bloqueado_por_semaforo(void * hilo) {
 	return (hilo_bloqueado->estado == ESTADO_BLOCK || hilo_bloqueado->estado == ESTADO_SUSPENDED_BLOCK) && hilo_bloqueado->bloqueante == SEMAFORO;
 }
 
-t_list * dfs(t_hilo * first_hilo, t_hilo * hilo_preguntar) {
+t_list * dfs(t_hilo * first_hilo, t_hilo * hilo_preguntar, int nivel) {
 	bool * visitado = dictionary_get(dict_visitados, string_from_format("%zu", pid(hilo_preguntar)));
 
 	if (*visitado) { return NULL; }
 	*visitado = true;
 
 	if (first_hilo == NULL) {
-		*first_hilo = hilo_preguntar;
+		first_hilo = hilo_preguntar;
 	} else if (pid(first_hilo) == pid(hilo_preguntar)) {
-		// Se cierra el ciclo. Encontro el deadlock
 		t_list * lista = list_create();
-		//list_add(lista, hilo_preguntar);
 
 		return lista;
 	}
 
-	// Procesos VECINOS
-	t_semaforo * semaforo_bloqueante = hilo_preguntar->bloqueante;
+	t_semaforo * semaforo_bloqueante = semaforo_get(hilo_preguntar->nombre_bloqueante);
+
 	t_list * procesos_ocupan_semaforo = list_filter(semaforo_bloqueante->list_procesos_retienen, esta_bloqueado_por_semaforo);
 
-	for (int i = 0; i < list_size(procesos_ocupan_semaforo); ++i) {
-		t_hilo * hilo_ocupan = list_get(procesos_ocupan_semaforo, i); // Hilo vecino [i]
+	char * tabs = string_repeat('\t', nivel);
+	loggear_trace("%s PID: %zu - SEM_BLOQUEANTE: %s - PROCESOS_RETIENEN: %d", tabs, hilo_preguntar->pid, semaforo_bloqueante->nombre, list_size(procesos_ocupan_semaforo));
 
-		t_list * lista = dfs(first_hilo, hilo_ocupan);
+	for (int i = 0; i < list_size(procesos_ocupan_semaforo); ++i) {
+		t_hilo * hilo_ocupan = list_get(procesos_ocupan_semaforo, i);
+
+		t_list * lista = dfs(first_hilo, hilo_ocupan, nivel + 1);
 		if (lista != NULL) {
-			// Significa que de donde viene, encontro deadlock.
 			list_add(lista, hilo_ocupan);
 
 			return lista;
@@ -77,17 +77,6 @@ t_list * dfs(t_hilo * first_hilo, t_hilo * hilo_preguntar) {
 	}
 
 	return NULL;
-}
-
-
-void ejemplo() {
-	iniciar_evaluacion_deadlock();
-	t_hilo * primer_hilo_bloqueado = list_get(list_bloqueados, 0);
-	t_list * hilos_deadlock = dfs(NULL, primer_hilo_bloqueado);
-	if (hilos_deadlock != NULL) {
-		loggear_warning("Hay deadlock");
-	}
-	destruir_evaluacion_deadlock();
 }
 
 int iniciar_control_deadlock();
@@ -111,13 +100,31 @@ int deadlocks_destruir() {
 }
 
 
-
 int deadlocks_ejecutar() {
 	while (true) {
 		usleep(ms_tiempo_deadlock * 1000),
 		loggear_trace("[DEADLOCK] - Pasaron %d milisegundos, controlamos deadlock", ms_tiempo_deadlock);
 
+		iniciar_evaluacion_deadlock();
 
+		if (list_size(list_bloqueados) == 0) {
+			loggear_trace("[DEADLOCK] - No hay procesos bloqueados");
+			destruir_evaluacion_deadlock();
+			continue;
+		}
+
+		t_hilo * primer_hilo_bloqueado = list_get(list_bloqueados, 0);
+		t_list * hilos_deadlock = dfs(NULL, primer_hilo_bloqueado, 1);
+
+		if (hilos_deadlock != NULL) {
+			loggear_warning("Hay un deadlock, los hilos son los siguientes");
+			for (int i = 0; i < list_size(hilos_deadlock); ++i) {
+				t_hilo * hilo = list_get(hilos_deadlock, i);
+				loggear_warning("PID: %zu", hilo->pid);
+			}
+		}
+
+		destruir_evaluacion_deadlock();
 	}
 }
 
