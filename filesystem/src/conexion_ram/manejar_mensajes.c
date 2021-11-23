63a5c27cd9332452051fb6b9cb95e4beba0c1f14
Alan <alan.anchart14@gmail.com>
2021-11-23 01:59:24 -0300

    agrego en el manejo de mensajes entre la swap y la ram el cas liberar_pagina

--- filesystem/src/conexion_ram/manejar_mensajes.c
+++ filesystem/src/conexion_ram/manejar_mensajes.c
@@ -132,10 +132,28 @@ int manejar_mensajes(t_prot_mensaje * mensaje) {
 		destruir_mensaje(mensaje);
 		return 0;
 	case R_S_ELIMINAR_PROCESO:
+		loggear_info("llego una peticion para eliminar un proceso"); //TODO necesito saber cual es el struct
 
 		return 0;
 	case R_S_LIBERAR_PAGINA:
+		loggear_info("llego una peticion liberar paginas");
+		mensaje_serializado = malloc(sizeof(t_pedir_o_liberar_pagina_s));
+		memcpy(mensaje_serializado, mensaje->payload, sizeof(t_mensaje_r_s));
+		t_pedir_o_liberar_pagina_s* liberar_deserializado = malloc(sizeof(t_pedir_o_liberar_pagina_s));
 
+		liberar_deserializado = deserializar_mensaje_peticion_liberacion_pagina(mensaje_serializado);
+		loggear_warning("la cantidad de paginas a liberar es %zu", liberar_deserializado->nro_pag);
+		carpincho  = buscar_carpincho_en_lista(liberar_deserializado->pid);
+		int error = borrar_x_cantidad_de_marcos(carpincho,  liberar_deserializado->nro_pag);
+		if(error < 0){
+			loggear_debug("OCURRIO UN ERROR INESPERADO AL QUERER LIBERAR LAS PAGINAS SOLICITIDAS PARA EL PROCESO %zu", liberar_deserializado->pid);
+			enviar_mensaje_protocolo(mensaje->socket, FALLO_EN_LA_TAREA, 0, NULL);
+		}
+
+		enviar_mensaje_protocolo(mensaje->socket, EXITO_EN_LA_TAREA, 0, NULL);
+		free(mensaje_serializado);
+		free(liberar_deserializado);
+		destruir_mensaje(mensaje);
 		return 0;;
 	case DESCONEXION_TOTAL:
 		loggear_error("Se cerró la conexión con ram");
